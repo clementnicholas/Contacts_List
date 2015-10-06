1b472bc68f293d5f4d0fe4617dbb380d089d5fb4
Nick Jordan <cnicholasjordan@gmail.com>
2015-10-06 21:31:31 +0000

    working prototype first try

--- contact.rb
+++ contact.rb
@@ -1,26 +1,43 @@
+require 'pry'
+
 class Contact
  
-  attr_accessor :name, :email, :contact_array
+  attr_accessor :name, :email
 
   def initialize(name, email)
     # TODO: assign local variables to instance variables
     @name = name
     @email = email
-    @contact_array = []
   end
  
   def to_s
     # 
   end
+
+
  
 
 
   ## Class Methods
   class << self
 
-    @@id = 0
 
-    def create(name, email)
+
+    @@id = 0
+    @@found = false
+    @@output = []
+
+    def add_to_output(obj)
+      if eval(obj[3]).empty?
+        string_to_add = "ID: #{obj[0]} \nNAME: #{obj[1]} \nEMAIL: #{obj[2]}"
+      else  
+        string_to_add = "ID: #{obj[0]} \nNAME: #{obj[1]} \nEMAIL: #{obj[2]} \n#{print_numbers(obj[3])}"
+      end
+      @@output << string_to_add if @@output.include?(string_to_add) == false
+      @@found = true
+    end
+
+    def create(name, email, phone_numbers)
       # TODO: Will initialize a contact as well as add it to the list of contacts
       @all_contacts = ContactDatabase.read_list
       @@id += @all_contacts.size + 1 
@@ -28,22 +45,21 @@ class Contact
       @contact_array << @@id
       @contact_array << name
       @contact_array << email
+      @contact_array << phone_numbers
       ContactDatabase.write_to_list(@contact_array)
+      "Contact with ID: #{@@id} created."
     end
  
     def find(term)
       # TODO: Will find and return contacts that contain the term in the first name, last name or email
-      included = false
-      output = ''
       ContactDatabase.read_list.each do |row|
         row.each do |str|
           if str.include?(term) 
-            output << "ID: #{row[0]} \nNAME: #{row[1]} \nEMAIL: #{row[2]} \n"
-            included = true
+            add_to_output(row)
           end
         end
       end
-      included == false ? contact_not_found : output
+      @@found == false ? contact_not_found : @@output
     end
  
     def all
@@ -54,9 +70,17 @@ class Contact
       end
     end
 
+    def print_numbers(obj)
+      @numbers_list = []
+      eval(obj).each do |key, value|
+        @numbers_list << "#{key.to_s.upcase}: #{value}"
+      end
+      @numbers_list.join(' ')
+    end
+
     def show_data(obj)
       string = ''
-      string << "#{obj[0]}: #{obj[1]} (#{obj[2]})"
+      string << "#{obj[0]}: #{obj[1]} (#{obj[2]}) #{print_numbers(obj[3])}" 
       puts string
     end
 
@@ -70,16 +94,13 @@ class Contact
 
     def show(id)
       # TODO: Show a contact, based on ID
-      found = false
-      output = ''
       contacts = ContactDatabase.read_list
       contact = contacts.find do |row| 
         if row[0] == id
-          output << "ID: #{row[0]} \nNAME: #{row[1]} \nEMAIL: #{row[2]} \n"
-          found = true
+          add_to_output(row)
         end
       end
-      found == false ? declare_invalid : output
+      @@found == false ? declare_invalid : @@output
     end
     
   end
--- contact_list.rb
+++ contact_list.rb
@@ -4,26 +4,66 @@ require_relative 'contact_database'
 # TODO: Implement command line interaction
 # This should be the only file where you use puts and gets
 
+def prompt_for_name
+  puts "Enter a name:"
+  @input_name = $stdin.gets.chomp
+end
 
-  puts "Enter a command:"
-  user_input = gets.chomp.downcase
+def prompt_for_email
+  puts "Enter an e-mail address:"
+  @input_email = $stdin.gets.chomp
+end
 
-  case user_input
+def get_phone_number
+  puts "Enter a type of phone_number:"
+  @type_of_number = $stdin.gets.chomp
+  puts "Enter phone_number:"
+  @phone_number = $stdin.gets.chomp
+end
+
+def prompt_for_phone
+  @numbers = {}
+  @response == 'YES'
+  until @response == 'NO' do
+  puts "Would you like to enter a phone_number? (YES or NO)"
+  @response = $stdin.gets.chomp.upcase
+    if @response == 'YES'
+      get_phone_number
+      @numbers[@type_of_number.to_sym] = @phone_number
+    end
+  end
+end
+
+def check_for_duplicate_email
+  ContactDatabase.read_list.each do |row|
+    if row[2] == @input_email
+      return true
+    end
+  end  
+end
+
+
+  command = ARGV
+  case command[0]
   when 'new'
-    puts "Enter a name:"
-    input_name = gets.chomp
-    puts "Enter an e-mail address:"
-    input_email = gets.chomp
-    new_contact = Contact.create(input_name, input_email)
+    prompt_for_email
+    if check_for_duplicate_email == true 
+      puts "I'm sorry, a contact with that email already exists."
+    else
+      prompt_for_name
+      prompt_for_phone
+      puts Contact.create(@input_name, @input_email, @numbers)
+    end
+
   when 'list'
     Contact.all
     puts "----------"
     puts "#{ContactDatabase.read_list.size} TOTAL CONTACTS"
-  when /^show\s\d*/
-    id_input = user_input.slice(/\d*\d$/)
+  when 'show'
+    id_input = command[1]
     puts Contact.show(id_input)
-  when /^find.*/
-    search_term = user_input.slice(/\s.*/).sub(/\s/, '')
+  when 'find'
+    search_term = command[1]
     puts Contact.find(search_term)
   when 'help'
     puts "Here is a list of available commands:
